fed19ccd7e36d851fa91e3ca1920e39b39044bc5
huchen <576175630@qq.com>
2022-05-06 16:12:32 +0800

    feat: init project

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = 'style-loader';
+const cssHandler = 'css-loader';
+
+const config = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].bundle.js',
+    chunkFilename: '[name].[hash].chunk.js',
+    publicPath: '/',
+  },
+  devServer: {
+    // open: true,
+    host: 'localhost',
+    historyApiFallback: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template/index.html',
+    }),
+    new HotModuleReplacementPlugin({}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, cssHandler],
+      },
+      {
+        test: /\.less$/i,
+        use: [stylesHandler, cssHandler, 'less-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
